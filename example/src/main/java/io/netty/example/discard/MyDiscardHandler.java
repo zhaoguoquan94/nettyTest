5f1a2dc72841c4868f6a961b6904ca75cf7361fc
Guoquan Zhao <zhaoguoquan94@gmail.com>
2016-03-14 09:08:27 +0800

    first commit:

---
+++ example/src/main/java/io/netty/example/discard/MyDiscardHandler.java
@@ -0,0 +1,24 @@
+package io.netty.example.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by guoquan on 16-3-13.
+ */
+public class MyDiscardHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        super.channelRead(ctx, msg);
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+//        super.exceptionCaught(ctx, cause);
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
---
+++ example/src/main/java/io/netty/example/discard/MyDiscardServer.java
@@ -0,0 +1,13 @@
+package io.netty.example.discard;
+
+/**
+ * Created by guoquan on 16-3-13.
+ */
+public class MyDiscardServer {
+    private int port;
+
+    public MyDiscardServer(int port) {
+        this.port = port;
+    }
+
+}
